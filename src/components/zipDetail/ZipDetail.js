23268ad9c970df9cea1b7da210c831a22d112aae
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-06 22:39:12 -0400

    switching zipdetail to reactstrap from bootstrap

--- src/components/zipDetail/ZipDetail.js
+++ src/components/zipDetail/ZipDetail.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import styles from './ZipDetail.module.css';
+import React from "react";
+import styles from "./ZipDetail.module.css";
+import { Card, CardTitle, ListGroup, ListGroupItem } from "reactstrap";
 
 class ZipDetail extends React.Component {
-    
-    render(){
-        return(
-            <div className={`${styles.sectionDetail}`}>
-                <label>Zip-code : {this.props.zipApiResponse.zip}</label><br/>
-                <label>County : {this.props.zipApiResponse.county}</label><br/>
-                <label>City : {this.props.city}</label><br/>
-                <label>State : {this.props.zipApiResponse.state}</label><br/>
-                <label>Country : {this.props.zipApiResponse.country}</label>
-            </div>
-        );
+  renderText() {
+    if (!this.props.zipApiResponse.zip) {
+      return;
+      // <div>Enter Zip code
+      // {/* {console.log(this.props.zipApiResponse)} */}
+      // </div>;
     }
+    return (
+      <div>
+        <Card body outline color="primary">
+          <CardTitle>ZIP code Details</CardTitle>
+          <ListGroup className="list-group">
+            <ListGroupItem className="list-group-item">
+              Zip-code : {this.props.zipApiResponse.zip}
+            </ListGroupItem>
+            <ListGroupItem className="list-group-item">
+              County : {this.props.zipApiResponse.county}
+            </ListGroupItem>
+            <ListGroupItem className="list-group-item">
+              City : {this.props.city}
+            </ListGroupItem>
+            <ListGroupItem className="list-group-item">
+              State : {this.props.zipApiResponse.state}
+            </ListGroupItem>
+            <ListGroupItem className="list-group-item">
+              Country : {this.props.zipApiResponse.country}
+            </ListGroupItem>
+          </ListGroup>
+        </Card>
+      </div>
+    );
+  }
+
+  render() {
+    return <div className={`${styles.sectionDetail}`}>{this.renderText()}</div>;
+  }
 }
 
 export default ZipDetail;
